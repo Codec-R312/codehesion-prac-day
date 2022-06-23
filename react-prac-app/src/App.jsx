3963e3afaac3f72ccb9e426776e7c360e93482f3
R.S. Maodi <silasmaodi@gmail.com>
2022-06-23 12:32:17 +0200

    ESLint added

--- react-prac-app/src/App.jsx
+++ react-prac-app/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
 
+import React from 'react';
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+import Home from './Components/Landing Page/Home';
+import Login from './Components/Login and Registration/Login';
+import Registration from './Components/Login and Registration/Registration';
+import Navbar from './Components/Navbar';
 
 function App() {
   
   return (
-    <div>
-    
-        <p>Hello Vite + React!</p>
-        
-    </div>
+    <Router>
+      <Navbar/>
+      <CssBaseline/>
+      <Routes>
+        <Route path={"/"} element={<Home/>}/>
+        <Route path={"/login"} element={<Login/>}/>
+        <Route path={"/registration"} element={<Registration/>}/>
+      </Routes>
+    </Router>
   )
 }
 
